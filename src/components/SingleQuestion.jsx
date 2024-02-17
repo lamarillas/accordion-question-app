f2f8c4f8b7c4c612dc002df45e27455504c34ac8
leo amarillas <lamarillas@vertex.mx>
2024-02-16 17:50:35 -0700

    other commit

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const SingleQuestion = ({question}) => {
+const SingleQuestion = ({title, info}) => {
+  const [show, setShow] = useState(false);
+
   return (
     <h2>SingleQuestion</h2>
   )
